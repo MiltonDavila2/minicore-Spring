483757b58954364994aedb187a3df133355b053b
MiltonDavila2 <miltondaviladt@gmail.com>
2025-04-16 21:49:34 -0500

    Commit inicial

---
+++ minicorecrud/src/main/java/com/debercrud/minicorecrud/MinicorecrudApplication.java
@@ -0,0 +1,13 @@
+package com.debercrud.minicorecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinicorecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinicorecrudApplication.class, args);
+	}
+
+}
---
+++ minicorecrud/src/test/java/com/debercrud/minicorecrud/MinicorecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debercrud.minicorecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinicorecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
